d82a69f0497864a22f632f8c85d1821d49724c97
JIAN TENG <aiyo321@gmail.com>
2022-12-02 13:31:16 +0800

    lint

--- day 1/b.ts
+++ day 1/b.ts
@@ -3,16 +3,17 @@ const input = await Deno.readTextFile("./input.txt");
 const array = input.split(/\n\n/);
 
 const sum = (i: string): number => {
-  return i.split(/\n/).reduce((prev,curr) => {
+  return i.split(/\n/).reduce((prev, curr) => {
     return prev + +curr;
-  }, 0)
-}
+  }, 0);
+};
 
-const sortedArr = array.reduce((prev, curr) => {
-  const elfCarrying = sum(curr);
+const sortedArr = array
+  .reduce((prev, curr) => {
+    const elfCarrying = sum(curr);
 
-  return [...prev, elfCarrying];
+    return [...prev, elfCarrying];
+  }, [] as number[])
+  .sort((a, b) => b - a);
 
-}, [] as number[]).sort((a, b) => b - a)
-
-console.log(sortedArr[0] + sortedArr[1] + sortedArr[2] );
+console.log(sortedArr[0] + sortedArr[1] + sortedArr[2]);
